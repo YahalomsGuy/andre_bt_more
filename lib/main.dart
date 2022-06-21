d17a1d089a15a4d91fcb20483f9e98c9616df18a
Guy Maimon <guy@yahaloms.com>
2022-06-21 22:33:36 +0400

    ui redesign, Long and short formats

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:blue_thermal_printer/blue_thermal_printer.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -29,30 +30,135 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   BlueThermalPrinter bluetooth = BlueThermalPrinter.instance;
-  late BluetoothDevice _device;
-  final String printerName = "XP-365B";
-  final String macAddress = "DC:0D:30:18:42:6B";
+  List<BluetoothDevice> _devices = [];
+  BluetoothDevice? _device;
+  bool switchOn = false;
+
   bool _connected = false;
 
   late String pathImage;
 
   @override
   void initState() {
-    _device = BluetoothDevice(printerName, macAddress);
-    _connected = _device.connected;
-    if (kDebugMode) {
-      print(_connected);
-    }
-    Map<dynamic, dynamic> deviceMap = _device.toMap();
-    print(deviceMap);
+    initPlatformState();
+    _connected = stillConnected();
+    setState(() {});
     super.initState();
+  }
+
+  bool stillConnected() {
+    bool? isOn = false;
+    bluetooth.isConnected.then((value) {
+      isOn = value;
+      return isOn;
+    });
+    return false;
+  }
+
+  Future<void> initPlatformState() async {
+    bool? isConnected = await bluetooth.isConnected;
+    List<BluetoothDevice> devices = [];
+    try {
+      devices = await bluetooth.getBondedDevices();
+    } on PlatformException {
+      if (kDebugMode) {
+        print("PlatformException!");
+      }
+    }
+
+    bluetooth.onStateChanged().listen((state) {
+      switch (state) {
+        case BlueThermalPrinter.CONNECTED:
+          setState(() {
+            _connected = true;
+            if (kDebugMode) {
+              print("bluetooth device state: connected");
+            }
+          });
+          break;
+        case BlueThermalPrinter.DISCONNECTED:
+          setState(() {
+            _connected = false;
+            if (kDebugMode) {
+              print("bluetooth device state: disconnected");
+            }
+          });
+          break;
+        case BlueThermalPrinter.DISCONNECT_REQUESTED:
+          setState(() {
+            _connected = false;
+            if (kDebugMode) {
+              print("bluetooth device state: disconnect requested");
+            }
+          });
+          break;
+        case BlueThermalPrinter.STATE_TURNING_OFF:
+          setState(() {
+            _connected = false;
+            if (kDebugMode) {
+              print("bluetooth device state: bluetooth turning off");
+            }
+          });
+          break;
+        case BlueThermalPrinter.STATE_OFF:
+          setState(() {
+            _connected = false;
+            if (kDebugMode) {
+              print("bluetooth device state: bluetooth off");
+            }
+          });
+          break;
+        case BlueThermalPrinter.STATE_ON:
+          setState(() {
+            _connected = false;
+            if (kDebugMode) {
+              print("bluetooth device state: bluetooth on");
+            }
+          });
+          break;
+        case BlueThermalPrinter.STATE_TURNING_ON:
+          setState(() {
+            _connected = false;
+            if (kDebugMode) {
+              print("bluetooth device state: bluetooth turning on");
+            }
+          });
+          break;
+        case BlueThermalPrinter.ERROR:
+          setState(() {
+            _connected = false;
+            if (kDebugMode) {
+              print("bluetooth device state: error");
+            }
+          });
+          break;
+        default:
+          if (kDebugMode) {
+            print(state);
+          }
+          break;
+      }
+    });
+
+    if (!mounted) return;
+    setState(() {
+      _devices = devices;
+    });
+
+    if (isConnected!) {
+      setState(() {
+        _connected = true;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text("Test")),
+        title: Center(
+          child: smartText("Bluetooth Printer Test"),
+        ),
       ),
       body: ListView(
         children: <Widget>[
@@ -60,30 +166,62 @@ class _MyHomePageState extends State<MyHomePage> {
             padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                const Text(
-                  'Device:',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
+              children: [
+                smartText('Device: \n Mac Address:',
+                    color: Colors.black, bold: true),
+                DropdownButton(
+                  items: _getDeviceItems(),
+                  onChanged: (BluetoothDevice? value) =>
+                      setState(() => _device = value),
+                  value: _device,
                 ),
-                Text(_device.name!),
-                ElevatedButton(
-                  onPressed: _connected ? _disconnect : _connect,
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                          _connected ? Colors.green : Colors.red)),
-                  child: Text(_connected ? 'On - Disconnect' : 'Off - Connect',
-                      style: const TextStyle(color: Colors.white)),
-                ),
+                //Text(_device.name!),
+                const SizedBox()
               ],
             ),
           ),
+          const SizedBox(height: 50),
           Padding(
-            padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 50),
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: ElevatedButton(
+              onPressed: _connected ? _disconnect : _connect,
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      _connected ? Colors.green : Colors.red)),
+              child: SizedBox(
+                height: 90,
+                child: smartText(
+                    _connected
+                        ? 'Printer Connected \nPress to disconnect \nexpect 1 beep....'
+                        : 'Printer disconnected \nPress to connect \nexpect 2 Beeps....',
+                    size: 24),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              smartText("print mode", color: Colors.black, size: 24),
+              CupertinoSwitch(
+                value: switchOn,
+                onChanged: (val) {
+                  setState(() => switchOn = val);
+                },
+              ),
+              smartText(switchOn ? "Long print" : "Short print",
+                  color: Colors.black, size: 24, bold: true)
+            ],
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
             child: ElevatedButton(
               onPressed: _connected ? _testPrint : null,
-              child: const Text('TesPrint'),
+              child: SizedBox(
+                height: 50,
+                child: smartText('Print Test', size: 30, bold: true),
+              ),
             ),
           ),
         ],
@@ -95,10 +233,12 @@ class _MyHomePageState extends State<MyHomePage> {
     bluetooth.isConnected.then((isConnected) {
       if (!isConnected!) {
         bluetooth
-            .connect(_device)
+            .connect(_device!)
             .then((value) => setState(() => _connected = true));
+        show("Connected!");
       } else {
         setState(() => _connected = true);
+        show("Was connected, UI updated");
       }
     });
   }
@@ -107,6 +247,7 @@ class _MyHomePageState extends State<MyHomePage> {
     bluetooth.disconnect().then(
           (value) => setState(() => _connected = false),
         );
+    show("Disconnected, UI updated");
   }
 
   void _testPrint() async {
@@ -120,49 +261,56 @@ class _MyHomePageState extends State<MyHomePage> {
     // 1- ESC_ALIGN_CENTER
     // 2- ESC_ALIGN_RIGHT
 
-    if (kDebugMode) {
-      print("Is it Connected? $_connected");
-    }
     if (_connected) {
-      bluetooth.printNewLine();
-      bluetooth.printCustom("HEADER", 3, 1);
-      bluetooth.printNewLine();
-      //bluetooth.printImage(pathImage); //path of your image/logo
-      bluetooth.printNewLine();
-      //bluetooth.printImageBytes(bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes));
-      bluetooth.printLeftRight("LEFT", "RIGHT", 0);
-      bluetooth.printLeftRight("LEFT", "RIGHT", 1);
-      bluetooth.printLeftRight("LEFT", "RIGHT", 1, format: "%-15s %15s %n");
-      bluetooth.printNewLine();
-      bluetooth.printLeftRight("LEFT", "RIGHT", 2);
-      bluetooth.printLeftRight("LEFT", "RIGHT", 3);
-      bluetooth.printLeftRight("LEFT", "RIGHT", 4);
-      bluetooth.printNewLine();
-      bluetooth.print3Column("Col1", "Col2", "Col3", 1);
-      bluetooth.print3Column("Col1", "Col2", "Col3", 1,
-          format: "%-10s %10s %10s %n");
-      bluetooth.printNewLine();
-      bluetooth.print4Column("Col1", "Col2", "Col3", "Col4", 1);
-      bluetooth.print4Column("Col1", "Col2", "Col3", "Col4", 1,
-          format: "%-8s %7s %7s %7s %n");
-      bluetooth.printNewLine();
-      String testString = " čĆžŽšŠ-H-ščđ";
-      bluetooth.printCustom(testString, 1, 1, charset: "windows-1250");
-      bluetooth.printLeftRight("Številka:", "18000001", 1,
-          charset: "windows-1250");
-      bluetooth.printCustom("Body left", 1, 0);
-      bluetooth.printCustom("Body right", 0, 2);
-      bluetooth.printNewLine();
-      bluetooth.printCustom("Thank You", 2, 1);
-      bluetooth.printNewLine();
-      bluetooth.printQRcode("Insert Your Own Text to Generate", 200, 200, 1);
-      bluetooth.printNewLine();
-      bluetooth.printNewLine();
-      bluetooth.paperCut();
-    } else {
-      if (kDebugMode) {
-        print("Printer Not Connected!!!");
+      try {
+        bluetooth.write("1 \r\n");
+        bluetooth.write("\n");
+        bluetooth.printNewLine();
+        bluetooth.printCustom("HEADER", 3, 1);
+        bluetooth.printNewLine();
+        //bluetooth.printImage(pathImage); //path of your image/logo
+        bluetooth.printNewLine();
+        //bluetooth.printImageBytes(bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes));
+        bluetooth.printLeftRight("LEFT", "RIGHT", 0);
+        bluetooth.printLeftRight("LEFT", "RIGHT", 1);
+        if (switchOn) {
+          bluetooth.printLeftRight("LEFT", "RIGHT", 1, format: "%-15s %15s %n");
+          bluetooth.printNewLine();
+          bluetooth.printLeftRight("LEFT", "RIGHT", 2);
+          bluetooth.printLeftRight("LEFT", "RIGHT", 3);
+          bluetooth.printLeftRight("LEFT", "RIGHT", 4);
+          bluetooth.printNewLine();
+          bluetooth.print3Column("Col1", "Col2", "Col3", 1);
+          bluetooth.print3Column("Col1", "Col2", "Col3", 1,
+              format: "%-10s %10s %10s %n");
+          bluetooth.printNewLine();
+          bluetooth.print4Column("Col1", "Col2", "Col3", "Col4", 1);
+          bluetooth.print4Column("Col1", "Col2", "Col3", "Col4", 1,
+              format: "%-8s %7s %7s %7s %n");
+          bluetooth.printNewLine();
+          String testString = " čĆžŽšŠ-H-ščđ";
+          bluetooth.printCustom(testString, 1, 1, charset: "windows-1250");
+          bluetooth.printLeftRight("Številka:", "18000001", 1,
+              charset: "windows-1250");
+          bluetooth.printCustom("Body left", 1, 0);
+          bluetooth.printCustom("Body right", 0, 2);
+          bluetooth.printNewLine();
+          bluetooth.printCustom("Thank You", 2, 1);
+          bluetooth.printNewLine();
+          bluetooth.printQRcode(
+              "Insert Your Own Text to Generate", 200, 200, 1);
+          bluetooth.printNewLine();
+          bluetooth.printNewLine();
+          bluetooth.paperCut();
+        }
+        show(switchOn
+            ? "Printed Long Format\n Finished successfully!"
+            : "Printed Short Format\n Finished successfully!");
+      } catch (e) {
+        show("Error: ${e.toString()}");
       }
+    } else {
+      show("Printer Disconnected");
     }
   }
 
@@ -170,16 +318,50 @@ class _MyHomePageState extends State<MyHomePage> {
     String message, {
     Duration duration = const Duration(seconds: 3),
   }) {
+    if (kDebugMode) {
+      print(message);
+    }
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(
+        backgroundColor: Colors.blue,
+        content: smartText(
           message,
-          style: const TextStyle(
-            color: Colors.white,
-          ),
+          size: 20,
         ),
         duration: duration,
       ),
     );
+  }
+
+  List<DropdownMenuItem<BluetoothDevice>> _getDeviceItems() {
+    List<DropdownMenuItem<BluetoothDevice>> items = [];
+    if (_devices.isEmpty) {
+      items.add(DropdownMenuItem(
+        child: smartText('NONE'),
+      ));
+    } else {
+      for (var device in _devices) {
+        items.add(DropdownMenuItem(
+          value: device,
+          child: smartText("${device.name!} \n ${device.address}",
+              color: Colors.black),
+        ));
+      }
+    }
+    return items;
+  }
+
+  Widget smartText(String text,
+      {double size = 18,
+      Color color = Colors.white,
+      bool bold = false,
+      TextAlign align = TextAlign.center}) {
+    return Text(text,
+        textAlign: align,
+        style: TextStyle(
+          fontSize: size,
+          color: color,
+          fontWeight: bold ? FontWeight.bold : FontWeight.normal,
+        ));
   }
 }
